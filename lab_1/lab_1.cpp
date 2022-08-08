571f73cc5c7d48667045ce16b4d846f0f2a84b9d
Ирина <@DESKTOP-MSGGBKL>
2022-08-09 00:14:35 +0500

    Добавьте файлы проекта.

---
+++ lab_1/lab_1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <сmath> //iostream как для ввода и вывода( это если просто), а cmath позволяет удобно работать с математикой
+using namespace std; / чтобы не писать std постоянно
+int main(int argc, const char* argv[]) {
+    int a = 1000;
+    float b = 0.0001, c, d, result; //double поменять на него для 2 части задания 
+    d = 2;
+    c = a - b;
+    result = ((pow(c, d) - (pow(a, d) * d * a * b)) / pow(b, d))
+        cout << result << endl; //вывод 
+    cin.get();
+    return 0;
+}
