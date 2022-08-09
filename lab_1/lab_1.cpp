89c4662271ed6ff18a44a72fcbaca3f308abc2cf
Irissha <110852608+Irissha@users.noreply.github.com>
2022-08-09 15:53:15 +0500

    Update lab_1.cpp

--- lab_1/lab_1.cpp
+++ lab_1/lab_1.cpp
@@ -1,12 +1,11 @@
-﻿#include <iostream>
-#include <сmath> //iostream как для ввода и вывода( это если просто), а cmath позволяет удобно работать с математикой
-using namespace std; // чтобы не писать std постоянно
-int main(int argc, const char* argv[]) {
-    int a = 1000;
-    float b = 0.0001, c, d, result; //double поменять на него для 2 части задания 
-    d = 2;
-    c = a - b;
-    result = ((pow(c, d) - (pow(a, d) * d * a * b)) / pow(b, d))
-        cout << result << endl; //вывод
-    return 0;
+#include <iostream>
+using namespace std;
+int main() {
+	int a = 1000;
+	float b = 0.0001, c, d, result;
+	d = 2;
+	c = a - b;
+	result = ((pow(c, d) - (pow(a, d) * d * a * b)) / pow(b, d));
+		cout << result << endl;
+	return 0;
 }
