1a29d1d37ad3ce51ce8cba5892615e04c5ef9ec7
Irissha <110852608+Irissha@users.noreply.github.com>
2022-09-22 23:07:09 +0500

    Update lab_1.cpp

--- lab_1/lab_1.cpp
+++ lab_1/lab_1.cpp
@@ -2,20 +2,23 @@
 #include <math.h> 
 using namespace std;
 
-int main() {
-	int a = 1000;
-	float b, result, d;
+int main()
+{
+	float a, b, result, d;
+	a = 1000;
 	b = 0.0001;
 	d = a - b;
+	
 	result = ((pow(d, 2)) - (pow(a, 2) - 2 * a * b)) / (pow(b, 2));
-	cout << "(float) result = " << result;
-	cout << "\n";
-	int a_1 = 1000;
-	double b_1, result_1, d_1;
+	cout << "(float) result = " << result << "\n";
+	
+	double b_1, result_1, d_1, a_1;
+	a_1 = 1000;
 	b_1 = 0.0001;
 	d_1 = a_1 - b_1;
+	
 	result_1 = ((pow(d_1, 2)) - (pow(a_1, 2) - 2 * a_1 * b_1)) / (pow(b_1, 2));
-	cout << "(double) result = " << result_1;
-	cout << "\n";
+	cout << "(double) result = " << result_1 << "\n";
+	
 	return 0;
 }
